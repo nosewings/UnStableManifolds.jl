da80b42db0dd5cf88c2619400f361d48e4d5c607
Nicholas Coltharp <nosewings@gmail.com>
2019-10-25 17:29:00 -0500

    updates iterates

--- src/UnStableManifolds.jl
+++ src/UnStableManifolds.jl
@@ -1,26 +1,25 @@
 module UnStableManifolds
 
+
 id(x...) :: Tuple = x
+
 
 function broadcast_size(arrays...) :: Tuple
     broadcasted = id.(arrays...)
-    if isa(broadcasted, Tuple)
-        (1,)
-    else
-        size(broadcasted)
-    end
+    broadcasted isa Tuple ? (1,) : size(broadcasted)
 end
+
 
 function iterates(
     f :: Function,
-    x,
+    x :: AbstractArray,
     args...,
     ;
     make :: Int = throw(ArgumentError),
     keep :: Union{Int, Nothing} = nothing,
     callback :: Union{Function, Nothing} = nothing,
     callback_depth :: Int = 0,
-)
+) :: Array
     if make < 0
         throw(ArgumentError)
     end
@@ -47,7 +46,9 @@ function iterates(
         slices[get_slice_index(i)]
     end
 
-    i = 1
+    # It's a good thing that the index variable in a for-loop is scoped, but I
+    # wish we could "remember" it if we wanted to.
+    i = 1 
     slices[1] .= x
     x = slices[1]
     while i <= make
@@ -56,25 +57,38 @@ function iterates(
         slice .= f.(x, args...)
         x = slice
         if callback != nothing
-            deep = tuple(map(get_slice, i-1 : -1 : i-(callback_depth+1))...)
+            deep = tuple(map(get_slice, i-1:-1:i-(callback_depth+1))...)
             callback(x, deep)
         end
     end
     i = get_slice_index(i)
-    cat(slices[i:end]..., slices[1:i-1]..., dims=length(buffer_size))
+    cat(slices[i+1:end]..., slices[1:i]..., dims=length(buffer_size))
 end
 
-# WHAT
-# BULLSHIT
-# IS
-# THIS
-(ricker_2d(
+
+# If we had higher-kinded types (and some more powerful static type expressions;
+# e.g., "strict subtype" or "is not abstract"), we could say "all of these
+# values need to be a subtype of a common concrete array type." Alas, we cannot.
+#
+# If we had a nontrivial least upper bound operator on types, we could at least
+# enforce that invariant dynamically. Alas, we cannot.
+#
+# So the only option is the lame one: just default to Array if x isn't an array.
+#
+# Why even bother with PLT if no one ever listens?
+function iterates(f :: Function, x, args...; kwargs...) :: Array
+    iterates(f, [x], args...; kwargs...)
+end        
+
+
+function ricker_2d(
     (x, y) :: Tuple{T, T},
     a :: T,
     b :: T,
     r :: T,
     s :: T,
-) :: Tuple{T, T}) where {T <: Real} =
+) :: Tuple{T, T} where {T <: Real}
     x*exp(r - x - a*y), y*exp(s - y - b*x)
+end
 
 end
